2bef80b3f3654fcf12f5b14434460db341debf53
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-07-05 11:11:04 -0600

    orginized the files

---
+++ my-bevs/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../styles/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <p>This is the APP level</p>
+    );
+  }
+}
+
+export default App;
