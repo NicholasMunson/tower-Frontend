06f203a977e12b8d3c24e6613efca98a560f597c
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-07-05 15:28:18 -0600

    added hamberger functionality

--- my-bevs/src/components/CustomNavBar.js
+++ my-bevs/src/components/CustomNavBar.js
@@ -1,34 +1,35 @@
 import React, {Component} from 'react'
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import {slide as Menu} from 'react-burger-menu'
 class CustomNavBar extends Component {
+    showSetting(event) {
+        event.preventDefault()
+
+
+    }
     render() {
         return (
-            <div>
+            <div className='header-container'>
                 <h1 className="my-bevs-title">
                     myBevs
                 </h1>
-                {/* <Navbar default="default" collapseOnSelect="collapseOnSelect">
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <Link to='/'>Drink Control</Link>
-                        </Navbar.Brand>
-                        <Navbar.Toggle/>
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav pullRight="pullRight">
-                            <NavItem eventKey={1} componentClass={Link} href='/beer-list' to='/beer-list'>
-                                BeerList
-                            </NavItem>
-                            <NavItem eventKey={2} componentClass={Link} href='/wine-list' to='/wine-list'>
-                                WineList
-                            </NavItem>
-                            <NavItem eventKey={3} componentClass={Link} href='/about' to='/about'>
-                                About
-                            </NavItem>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar> */}
+                <Menu right >
+                    <Nav pullRight="pullRight">
+                        <NavItem eventKey={1} componentClass={Link} href='/beer-list' to='/beer-list'>
+                            BeerList
+                        </NavItem>
+                        <NavItem eventKey={2} componentClass={Link} href='/wine-list' to='/wine-list'>
+                            WineList
+                        </NavItem>
+                        <NavItem eventKey={3} componentClass={Link} href='/add' to='/add'>
+                            Add Bevs
+                        </NavItem>
+                        <NavItem eventKey={4} componentClass={Link} href='/about' to='/about'>
+                            About
+                        </NavItem>
+                    </Nav>
+                </Menu>
             </div>
         )
     }
--- my-bevs/src/components/Header.js
+++ my-bevs/src/components/Header.js
@@ -1,6 +1,3 @@
-// import React, {Component} from 'react';
-// import Nav from './CustomNavBar'
-
 import React from 'react';
 import CustomNavBar from './CustomNavBar'
 
