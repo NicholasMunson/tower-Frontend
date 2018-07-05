93ebf667ae7a5341efb9deffee857e1e8a45437a
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-07-05 17:02:43 -0600

    style the hamburger menu items

--- my-bevs/src/components/CustomNavBar.js
+++ my-bevs/src/components/CustomNavBar.js
@@ -2,30 +2,42 @@ import React, {Component} from 'react'
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import {slide as Menu} from 'react-burger-menu'
+import '../styles/hamburger.css'
 class CustomNavBar extends Component {
-    showSetting(event) {
-        event.preventDefault()
 
-
-    }
     render() {
         return (
-            <div className='header-container'>
-                <h1 className="my-bevs-title">
-                    myBevs
-                </h1>
+            <div className='header-container right'>
                 <Menu right >
-                    <Nav pullRight="pullRight">
-                        <NavItem eventKey={1} componentClass={Link} href='/beer-list' to='/beer-list'>
+                    <Nav pullRight>
+                        <NavItem 
+                            eventKey={1} 
+                            componentClass={Link} 
+                            href='/beer-list' 
+                            to='/beer-list' 
+                            className='nav-item'>
                             BeerList
                         </NavItem>
-                        <NavItem eventKey={2} componentClass={Link} href='/wine-list' to='/wine-list'>
+                        <NavItem 
+                            eventKey={2} 
+                            componentClass={Link} 
+                            href='/wine-list' 
+                            to='/wine-list' 
+                            className='nav-item'>
                             WineList
                         </NavItem>
-                        <NavItem eventKey={3} componentClass={Link} href='/add' to='/add'>
+                        <NavItem 
+                            eventKey={3} 
+                            componentClass={Link} 
+                            href='/add' to='/add' 
+                            className='nav-item'>
                             Add Bevs
                         </NavItem>
-                        <NavItem eventKey={4} componentClass={Link} href='/about' to='/about'>
+                        <NavItem eventKey={4}
+                            componentClass={Link} 
+                            href='/about' 
+                            to='/about' 
+                            className='nav-item'>
                             About
                         </NavItem>
                     </Nav>
--- my-bevs/src/components/Footer.js
+++ my-bevs/src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     render() {
         return ( 
             <div className='footer-container'>
-                <p>myBevs</p>
+                <small>myBevs® 2018</small>
             </div>
             
         )
