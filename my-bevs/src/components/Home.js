f690a7513d2bce6065d75223840b48d180818864
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-07-07 22:05:15 -0600

    remove console.log from home

--- my-bevs/src/components/Home.js
+++ my-bevs/src/components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Home = (props) => {
-    console.log("this one", props)
     return (
         <div className='home-container'>
             <p>Welcome to the home console</p>
