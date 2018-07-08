b2e5e89932a4c6002aa7d9b6bd0da32e5f12666e
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-07-07 22:04:35 -0600

    cleaned up the code on home.js

--- my-bevs/src/components/Home.js
+++ my-bevs/src/components/Home.js
@@ -6,23 +6,7 @@ const Home = (props) => {
         <div className='home-container'>
             <p>Welcome to the home console</p>
         </div>
-
     )
 }
 
-// class Home extends Component {
-//     render() {
-//         return (
-//             <div className='home-container'>
-//                 <p>Welcome to the home console</p>
-//                 <p>pie chart with ratio of beers to wine goes here</p>
-//                 <p>pie chart with ratio of beers to wine goes here</p>
-//                 <p>pie chart with ratio of beers to wine goes here</p>
-//                 <p>pie chart with ratio of beers to wine goes here</p>
-//             </div>
-
-//         )
-//     }
-
-// }
 export default Home
