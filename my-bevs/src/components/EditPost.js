be8b823c0bab5e5582396b9824ee0694955197ed
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-07-11 22:35:17 -0600

    inport css

--- my-bevs/src/components/EditPost.js
+++ my-bevs/src/components/EditPost.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import { FormGroup, FormControl, Button } from 'react-bootstrap'
-import '../styles/add.css'
+import '../styles/edit.css'
 class EditPost extends Component {
     constructor(props){
         super(props)
@@ -58,68 +58,71 @@ class EditPost extends Component {
 y
     render() {
         return (
-            <form 
-                // onSubmit={this.handleSubmit} 
-                onSubmit={this.props.handleDisplayChangeBack} 
-                className="form-container" 
-                // key={this.id}
-                >
-                <FormGroup>
-                    <FormControl
-                        type="text"
-                        value={this.brewery}
-                        placeholder="Brewery Name"
-                        onChange={this.handleChange}
-                        required
-                        name="brewery"/>
-                    <FormControl
-                        type="text"
-                        value={this.name}
-                        placeholder="Beer Name"
-                        onChange={this.handleChange}
-                        required
-                        name="name"/>
-                    <FormControl
-                        type="text"
-                        value={this.style}
-                        placeholder="Style"
-                        onChange={this.handleChange}
-                        required
-                        name="style"/>
-                    <FormControl
-                        type="number"
-                        step="any"
-                        value={this.ABV}
-                        placeholder="ABV"
-                        onChange={this.handleChange}
-                        required
-                        name="ABV"/>
-                    <FormGroup controlId="formControlsSelect">
-                    <FormControl 
-                        required
-                        componentClass="select"  
-                        value={this.rating}
-                        onChange={this.handleChange}
-                        name="rating">
-                            <option type='text' default>Beer Rating</option>
-                            <option type='number' value="1">1</option>
-                            <option type='number' value="2">2</option>
-                            <option type='number' value="3">3</option>
-                            <option type='number' value="4">4</option>
-                            <option type='number' value="5">5</option>
-                    </FormControl>
-                    <FormControl 
-                        type="text" 
-                        componentClass="textarea" 
-                        name="notes" 
-                        onChange={this.handleChange}  
-                        value={this.notes} 
-                        placeholder="Notes" />
-                    </FormGroup>    
-                </FormGroup>
-                <Button type="submit">Submit</Button>
-            </form>
-            
+            <Fragment> 
+                <h4 className="form-title">Edit your Bev</h4>
+                <form 
+                    // onSubmit={this.handleSubmit} 
+                    onSubmit={this.props.handleDisplayChangeBack} 
+                    className="edit-form-container" 
+                    // key={this.id}
+                    >
+                    <FormGroup>
+                        <FormControl
+                            type="text"
+                            value={this.brewery}
+                            placeholder="Brewery Name"
+                            onChange={this.handleChange}
+                            required
+                            name="brewery"/>
+                        <FormControl
+                            type="text"
+                            value={this.name}
+                            placeholder="Beer Name"
+                            onChange={this.handleChange}
+                            required
+                            name="name"/>
+                        <FormControl
+                            type="text"
+                            value={this.style}
+                            placeholder="Style"
+                            onChange={this.handleChange}
+                            required
+                            name="style"/>
+                        <FormControl
+                            type="number"
+                            step="any"
+                            value={this.ABV}
+                            placeholder="ABV"
+                            onChange={this.handleChange}
+                            required
+                            name="ABV"/>
+                        <FormGroup controlId="formControlsSelect">
+                        <FormControl 
+                            required
+                            componentClass="select"  
+                            value={this.rating}
+                            onChange={this.handleChange}
+                            name="rating">
+                                <option type='text' default>Beer Rating</option>
+                                <option type='number' value="1">1</option>
+                                <option type='number' value="2">2</option>
+                                <option type='number' value="3">3</option>
+                                <option type='number' value="4">4</option>
+                                <option type='number' value="5">5</option>
+                        </FormControl>
+                        <FormControl 
+                            type="text" 
+                            componentClass="textarea" 
+                            name="notes" 
+                            onChange={this.handleChange}  
+                            value={this.notes} 
+                            placeholder="Notes" />
+                        </FormGroup>    
+                    </FormGroup>
+                    <Button className="btn-primary" type="submit">Submit</Button>
+                    <Button className="btn-primary">Back</Button>
+                </form>
+            </Fragment>
         )
     }
 
