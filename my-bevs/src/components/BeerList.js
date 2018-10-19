8b92c7d6abbe24772ff6f5b739d4345491cb7e39
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-10-18 20:42:06 -0600

    removed console.log

--- my-bevs/src/components/BeerList.js
+++ my-bevs/src/components/BeerList.js
@@ -3,7 +3,6 @@ import BeerCard from '../components/BeerCard.js'
 import '../styles/card-container.css'
 
 const BeerList = (props) => {
-    console.log(props)
     const beerCard = props.beerData.map(beer => <BeerCard handleDisplayChange={props.handleDisplayChange} handleBeerDelete={props.handleBeerDelete}  updateBevCard={props.updateBevCard} beer={beer} key={beer.id}/>)
     const revBeerCard = beerCard.reverse()
     return(
