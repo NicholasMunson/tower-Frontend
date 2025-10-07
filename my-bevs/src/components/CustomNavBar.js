9f926469aa504551cb6500a11bfc4d94a88e47af
nicholasmunson <nicholas.t.munson@gmail.com>
2025-10-06 23:34:40 -0600

    Enhance CustomNavBar: Introduce state management for menu visibility and update link click handling to close the menu, improving user experience and navigation flow.

--- my-bevs/src/components/CustomNavBar.js
+++ my-bevs/src/components/CustomNavBar.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import "../styles/hamburger.css";
 
 const CustomNavBar = (props) => {
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuStateChange = (state) => {
+    setIsMenuOpen(state.isOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <div className="header-container right">
-      <Menu right>
+      <Menu right isOpen={isMenuOpen} onStateChange={handleMenuStateChange}>
         <Link
           to="/"
           className={`menu-item ${location.pathname === "/" ? "active" : ""}`}
+          onClick={handleLinkClick}
         >
           About
         </Link>
@@ -20,6 +30,7 @@ const CustomNavBar = (props) => {
           className={`menu-item ${
             location.pathname === "/home" ? "active" : ""
           }`}
+          onClick={handleLinkClick}
         >
           Home
         </Link>
@@ -28,6 +39,7 @@ const CustomNavBar = (props) => {
           className={`menu-item ${
             location.pathname === "/beer-list" ? "active" : ""
           }`}
+          onClick={handleLinkClick}
         >
           BeerList
         </Link>
@@ -36,6 +48,7 @@ const CustomNavBar = (props) => {
           className={`menu-item ${
             location.pathname === "/wine-list" ? "active" : ""
           }`}
+          onClick={handleLinkClick}
         >
           WineList
         </Link>
@@ -44,6 +57,7 @@ const CustomNavBar = (props) => {
           className={`menu-item ${
             location.pathname === "/add" ? "active" : ""
           }`}
+          onClick={handleLinkClick}
         >
           Add-Bevs
         </Link>
